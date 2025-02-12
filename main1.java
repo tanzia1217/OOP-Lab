f76005619b94c6fb21ef4a26005c490ca25c6f5f
tanzia1217 <rouf2305101217@diu.edu.bd>
2025-02-12 22:19:11 +0600

    Add files via upload

---
+++ main1.java
@@ -0,0 +1,12 @@
+public class main1
+{
+    public static void main(String[] args) {
+
+        HR obj2=new HR();
+        obj2.display();
+
+        software_engineer obj1=new software_engineer();
+        obj1.display();
+
+    }
+}
