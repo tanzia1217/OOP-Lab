730907e4059c367a0b4853ca6d8f8253c678c04a
tanzia1217 <rouf2305101217@diu.edu.bd>
2025-04-16 22:38:55 +0600

    Add files via upload

---
+++ InvalidVoterIDException.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class InvalidVoterIDException extends Exception {
+    InvalidVoterIDException(String message) {
+        super(message);
+    }
+
+}
+
+
+ class ExceptionHandling {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Your Age : ");
+        int age = scanner.nextInt();
+        try {
+            if (age < 18) {
+                throw new InvalidVoterIDException("This is InvalidVoterIDException");
+            }
+            else {
+                System.out.println("You are Above 18.");
+            }
+        }
+        catch (InvalidVoterIDException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
