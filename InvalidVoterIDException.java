e71d64283c93fdc140f265d6eb0c079ecd53acf9
tanzia1217 <rouf2305101217@diu.edu.bd>
2025-04-16 22:41:59 +0600

    InvalidVoterIDException.java

--- InvalidVoterIDException.java
+++ InvalidVoterIDException.java
@@ -7,7 +7,6 @@ class InvalidVoterIDException extends Exception {
 
 }
 
-
  class ExceptionHandling {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
